f9a1e8c72e85908c570a582313e579e8f3c769b5
ZION <warmth.giver@gmail.com>
2024-12-02 18:01:12 +0900

    Project Snake Start
    프로젝트 스네이크 시작

--- 강의/24.12.02 (009일 차)/과제 (심화)/Assignment1/Program.cs
+++ 강의/24.12.02 (009일 차)/과제 (심화)/Assignment1/Program.cs
@@ -33,7 +33,8 @@ namespace Assignment1
                 "속성으로 Weapon구조체를 3개 담을 수 있는 배열 및" +
                 "현재 손에 들고 있는 Weapon이 무엇인지 저장할 배열의 인덱스로 사용할 정수형 변수 하나를 작성합니다.\n" +
                 "- 메인에 돌아와서 솔져 구조체를 하나 만들어줍니다." +
-                "솔져가 가진 무기의 배열에 본인이 희망하는 이름을 담은 무기 3가지를 모두 기입하여 줍니다.\n" +
+                "솔져가 가진 무기의 배열에 본인이 희망하는 이름을 담은 무기 3가지를 모두 기입하여 줍니다." +
+                "처음엔 1번을 들고 있습니다(인간의 말로)\n" +
                 "- 함수를 하나 만들되, 인자값으로 정수형과 솔져형 두가지를 받는 void형 함수를 제작할 것입니다. 함수의 이름은 ChangeWeapon으로 하고 기능은 다음과 같습니다.\n" +
                 "    - 함수가 호출되면 배열 속의 Weapon 이름들을 전부 출력하고 다음줄에 어떤 무기로 바꿀것인지 콘솔에 적습니다.\n" +
                 "    - 유저로부터 1~3사이 정수를 입력받되 1~3이 아닐 경우, 제대로 1~3사이 정수를 입력하라고 콘솔에 입력 후 무한으로 재입력을 요구합니다.\n" +
@@ -45,7 +46,7 @@ namespace Assignment1
                 "        - 정상적으로 무기 변경이 일어날 수 있는 상황.";
 
             // 메인에 돌아와서 솔져 구조체를 하나 만들어줍니다.
-            Soilder soilder1 = new()
+            Soilder soilder = new()
             {
                 // 솔져가 가진 무기의 배열에 본인이 희망하는 이름을 담은 무기 3가지를 모두 기입하여 줍니다.
                 weapons =
@@ -58,17 +59,12 @@ namespace Assignment1
                 currentWeapon = 1,
             };
 
-            // 시작 시 본인이 들고 있는 무기와 일치하는 숫자를 유저가 입력한 상황.
-            ChangeWeapon(soilder1);
-            // 1~3이 아닌 숫자를 입력한 상황.
-            ChangeWeapon(soilder1);
-            // 정상적으로 무기 변경이 일어날 수 있는 상황.
-            ChangeWeapon(soilder1);
+            ChangeWeapon(ref soilder);
         }
 
         // 함수를 하나 만들되, 인자값으로 정수형과 솔져형 두가지를 받는 void형 함수를 제작할 것입니다.
         // 함수의 이름은 ChangeWeapon으로 하고 기능은 다음과 같습니다.
-        private static void ChangeWeapon(Soilder soilder)
+        private static void ChangeWeapon(ref Soilder soilder)
         {
             // 함수가 호출되면 배열 속의 Weapon 이름들을 전부 출력하고
             for (int i = 0; i < soilder.weapons.Length; ++i)
@@ -78,7 +74,7 @@ namespace Assignment1
                 Console.WriteLine();
             }
 
-            int cindex;
+            int weaponNumber;
 
             while (true)
             {
@@ -86,10 +82,10 @@ namespace Assignment1
                 Console.WriteLine();
                 Console.Write($"바꿀 무기 입력(1~{soilder.weapons.Length}): ");
                 // 유저로부터 1~3사이 정수를 입력받되
-                int.TryParse(Console.ReadLine(), out cindex);
+                int.TryParse(Console.ReadLine(), out weaponNumber);
 
                 // 1~3이 아닐 경우,
-                if (cindex < 1 || soilder.weapons.Length < cindex)
+                if (weaponNumber < 1 || soilder.weapons.Length < weaponNumber)
                 {
                     // 제대로 1~3사이 정수를 입력하라고 콘솔에 입력 후 무한으로 재입력을 요구합니다.
                     Console.WriteLine();
@@ -98,8 +94,9 @@ namespace Assignment1
 
                     continue;
                 }
+
                 // 제대로 1,2,3 중 하나를 입력 받았을 경우, 만약 본인의 손에 들고 있는 무기와 같은 무기를 입력하였어도
-                else if (cindex == soilder.currentWeapon)
+                if (weaponNumber == soilder.currentWeapon)
                 {
                     // '현재 들고 있는 무기와 동일합니다' 를 출력 후,
                     Console.WriteLine();
@@ -113,12 +110,12 @@ namespace Assignment1
                 break;
             }
 
+            soilder.currentWeapon = weaponNumber;
+
             // 본인 손에 들고 있는 무기와는 다른 무기를 정상적으로 골랐다면 콘솔 출력을 통해 해당 무기로 바뀌었음을 출력합니다.
             Console.WriteLine();
-            Console.Write($"무기가 {soilder.weapons[cindex].name}로 바뀌었습니다.");
+            Console.Write($"무기가 {soilder.weapons[weaponNumber - 1].name}로 바뀌었습니다.");
             Console.WriteLine();
-
-            // 이후 메인 문에 ChangeWeapon 함수를 3번 실행하되, 아래 각각의 상황에 맞게 호출하세요.
         }
 
         // Weapon 이라는 구조체를 하나 만든 후,
---
+++ 프로젝트/Snake/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Snake
+{
+    internal class Program
+    {
+        const int windowWidth = 65;
+        const int windowHeight = 33;
+
+        private static void Main(string[] args)
+        {
+            Console.SetWindowSize(windowWidth, windowHeight);
+
+            while (true)
+            {
+                Console.Clear();
+
+                Console.Write("                   ###  #   #  ###  #   # #####                  \n");
+                Console.Write("                  #     ##  # #   # #  #  #                      \n");
+                Console.Write("#################  ###  # # # ##### ###   ##### #################\n");
+                Console.Write("                      # #  ## #   # #  #  #                      \n");
+                Console.Write("                   ###  #   # #   # #   # #####                  \n");
+
+                Console.ReadLine();
+            }
+        }
+
+        private class Snake
+        {
+
+        }
+    }
+}
