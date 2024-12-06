60125f9ecca500e5eebfccad4ed7a7ea4ffa2f1c
ZION <warmth.giver@gmail.com>
2024-12-06 18:03:29 +0900

    Day 013
    13일 차

---
+++ 강의/24.12.04 (011일 차)/강의 노트/Example1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Example1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //StopwatchExample1();
+            KeyAvailableExample1();
+        }
+
+        private static void StopwatchExample1()
+        {
+            // Stopwatch
+            // 
+
+            Stopwatch stopwatch = new();
+
+            stopwatch.Start();
+
+            while (true)
+            {
+                Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            }
+        }
+
+        private static void KeyAvailableExample1()
+        {
+            // Console.KeyAvailable
+            // 
+
+            while (true)
+            {
+                Console.Write('.');
+
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey(true).Key;
+
+                    Console.Write(key);
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
